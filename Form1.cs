8b281602404e95f12da1e99b2169dfc0918e0104
Nixie <mgmbautista@gmail.com>
2024-10-10 16:12:30 -0600

    Documentacion Interna

--- Form1.cs
+++ Form1.cs
@@ -8,12 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+// Programador: Equipo de Desarrollo (NixieNixi,RaRMustis,CanelaFeliz,3smer4ld4)
+// Proyecto: Sistema para Veterinaria "Cat-Dog"
+// Fecha de Diseño: Octubre de 2024
+
 namespace Clave1_GrupoDeTrabajo1
-{
+{ 
+    
+    //Formulario Principal de la Aplicacion.
     public partial class Form1 : Form
     {
+        //Constructor del formulario principal.
         public Form1()
         {
+            // Inicialización de los componentes visuales del formulario
             InitializeComponent();
         }
 
@@ -26,21 +35,32 @@ namespace Clave1_GrupoDeTrabajo1
             //se usa la propiedad ShowDialog del objeto creado para abrir el nuevo form
             inicio.ShowDialog();
         }
-        
+
+        // Este método debería abrir el formulario donde el veterinario podrá iniciar sesión.
+        // El veterinario tiene permisos para registrar, consultas y administrar el cuadro clínico de las mascotas.
         private void btnIngresoVete_Click(object sender, EventArgs e)
         {
-            //Aqui iria el para abrir donde el user vete realizara su login.(NixieNixi)
+            // Aquí se implementará la apertura del formulario donde el veterinario realizará su login.
+            // Rol: Veterinario
+            // Responsabilidad: Registrar consultas, manejar cuadro clínico y administrar vacunas.
         }
 
+        // Este método debería abrir el formulario donde el administrador del sistema inicia sesión.
+        // El administrador podrá gestionar las citas, productos, y pagos de la veterinaria.
         private void btnIngresoAdmin_Click(object sender, EventArgs e)
         {
-            //Aqui iria el para abrir donde el user Administrador realizara su login.(NixieNixi)
-
+            // Aquí se implementará la apertura del formulario donde el administrador realizará su login.
+            // Rol: Administrador
+            // Responsabilidad: Gestionar citas, modificar registros, y controlar los pagos.
         }
 
+        // Este método debería abrir el formulario donde el dueño de la mascota inicia sesión.
+        // El dueño podrá programar, modificar o cancelar citas, además de realizar compras de productos.
         private void btnIngresoDueno_Click(object sender, EventArgs e)
         {
-            //Aqui iria el para abrir donde el user Dueño  realizara su login.(NixieNixi)
+            // Aquí se implementará la apertura del formulario donde el dueño realizará su login.
+            // Rol: Dueño de la Mascota
+            // Responsabilidad: Programar, cancelar o modificar citas y realizar compras.
 
         }
     }
