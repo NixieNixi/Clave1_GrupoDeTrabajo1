81e7d0ec997073af6508307bfbcb630d5e27956f
Galleta <cristian.vas0304@gmail.com>
2024-10-24 20:10:19 -0600

    Diseño formulario dueño - usuarios

---
+++ Administrador/AdministradorPerfil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clave1_GrupoDeTrabajo1.Administrador
+{
+    public partial class AdministradorPerfil : Form
+    {
+        public AdministradorPerfil()
+        {
+            InitializeComponent();
+            txtIdUsuario.Text = "A10000";
+            txtUsuario.Text="canela.feliz";
+            txtNombre.Text = "Galleta";
+            txtTelefono.Text = "60506482";
+            txtEmail.Text = "galletadecanela2048@gmail.com";
+            txtDireccion.Text = "Comunidad Adesco Flores, Sector #3, Casa #44, Ilopango";
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            MenuPrincipal menu = new MenuPrincipal();
+            this.Hide();
+            menu.ShowDialog();
+        }
+    }
+}
--- Interfaz/Login.cs
+++ Interfaz/Login.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Clave1_GrupoDeTrabajo1;
+using Clave1_GrupoDeTrabajo1.Administrador;
 using Clave1_GrupoDeTrabajo1.Interfaz;
 
 namespace Clave1_GrupoDeTrabajo1
@@ -46,7 +47,9 @@ namespace Clave1_GrupoDeTrabajo1
         //Metodo para abrir el perfil del Administrador
         private void btnAdmin_Click(object sender, EventArgs e)
         {
-
+            AdministradorPerfil administrador = new AdministradorPerfil();
+            this.Hide();
+            administrador.ShowDialog();
         }
     }
 }
