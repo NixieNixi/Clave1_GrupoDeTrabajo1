8853cfc308c5779860f0c8ef6bcd0ccb9a1e02c3
Galleta <cristian.vas0304@gmail.com>
2024-10-27 00:07:37 -0600

    Base de datos formal

--- sql/clave1_grupodetrabajodb1.sql
+++ sql/clave1_grupodetrabajodb1.sql
@@ -23,19 +23,16 @@ DROP TABLE IF EXISTS `cirugia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cirugia` (
-  `idcirugia` int NOT NULL AUTO_INCREMENT,
-  `Tipo` varchar(100) NOT NULL,
-  `Descripcion` longtext,
-  `Motivo` varchar(255) DEFAULT NULL,
-  `Materiales` longtext,
-  `idExpediente` int NOT NULL,
+  `idCirugia` int NOT NULL AUTO_INCREMENT,
   `idMascota` int NOT NULL,
-  PRIMARY KEY (`idcirugia`),
-  KEY `idExpediente_cirugia` (`idExpediente`),
-  KEY `idMascota_cirugia` (`idMascota`),
-  CONSTRAINT `idExpediente_cirugia` FOREIGN KEY (`idExpediente`) REFERENCES `expedientes` (`idExpedientes`),
-  CONSTRAINT `idMascota_cirugia` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Tipo` enum('Castracion','Extraccion de objeto','Absceso','Lavado gastrico','Cesarea') NOT NULL,
+  `Descripcion` longtext NOT NULL,
+  `Motivo` varchar(255) NOT NULL,
+  `Materiales` longtext NOT NULL,
+  PRIMARY KEY (`idCirugia`),
+  KEY `fk_idMascotaCirugia_idx` (`idMascota`),
+  CONSTRAINT `idMascotaCirugia` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +41,7 @@ CREATE TABLE `cirugia` (
 
 LOCK TABLES `cirugia` WRITE;
 /*!40000 ALTER TABLE `cirugia` DISABLE KEYS */;
+INSERT INTO `cirugia` VALUES (1,1,'Castracion','Esteriliazcion Misi','Motivo valido','Material1');
 /*!40000 ALTER TABLE `cirugia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +63,7 @@ CREATE TABLE `citas` (
   KEY `idUsuario_Citas_idx` (`idUsuarios`),
   KEY `idMascota_Citas` (`idMascota`),
   CONSTRAINT `idMascota_Citas` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`),
-  CONSTRAINT `idUsuario_Citas` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`idUsuarios`)
+  CONSTRAINT `idUsuario_Citas` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`idUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -87,6 +85,7 @@ DROP TABLE IF EXISTS `consultas`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `consultas` (
   `idConsulta` int NOT NULL AUTO_INCREMENT,
+  `idMascota` int NOT NULL,
   `FechaHora` datetime NOT NULL,
   `Peso` decimal(5,2) NOT NULL,
   `Motivo` varchar(255) DEFAULT NULL,
@@ -96,14 +95,10 @@ CREATE TABLE `consultas` (
   `Tratamiento` varchar(1000) DEFAULT NULL,
   `Medicamentos` varchar(100) DEFAULT NULL,
   `Notas` varchar(1000) DEFAULT NULL,
-  `idMascota` int NOT NULL,
-  `idExpediente` int NOT NULL,
   PRIMARY KEY (`idConsulta`),
-  KEY `idExpediente_idx` (`idExpediente`),
-  KEY `idMascota_Consulta_idx` (`idMascota`),
-  CONSTRAINT `idExpediente` FOREIGN KEY (`idExpediente`) REFERENCES `expedientes` (`idExpedientes`),
-  CONSTRAINT `idMascota_Consulta` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `idMascotaConsulta_idx` (`idMascota`),
+  CONSTRAINT `idMascotaConsulta` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,6 +107,7 @@ CREATE TABLE `consultas` (
 
 LOCK TABLES `consultas` WRITE;
 /*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
+INSERT INTO `consultas` VALUES (1,1,'2024-01-30 15:00:00',2.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,1,'2024-09-12 10:00:00',2.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(4,1,'2024-02-12 07:00:00',2.00,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,19 +119,16 @@ DROP TABLE IF EXISTS `examen`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `examen` (
-  `idexamen` int NOT NULL AUTO_INCREMENT,
-  `Tipo` varchar(100) NOT NULL,
-  `Descripcion` longtext,
-  `Motivo` varchar(255) DEFAULT NULL,
-  `Materiales` mediumtext,
-  `idExpediente` int NOT NULL,
+  `idExamen` int NOT NULL AUTO_INCREMENT,
   `idMascota` int NOT NULL,
-  PRIMARY KEY (`idexamen`),
-  KEY `idExpedientes` (`idExpediente`),
-  KEY `fk_idMascota_nuevo` (`idMascota`),
-  CONSTRAINT `fk_idExpedientes` FOREIGN KEY (`idExpediente`) REFERENCES `expedientes` (`idExpedientes`),
-  CONSTRAINT `fk_idMascota_nuevo` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Tipo` enum('Sangre','Sida felino','Leucemia felino','Radiografia','Ultrasonido') NOT NULL,
+  `Descripcion` longtext NOT NULL,
+  `Motivo` varchar(255) NOT NULL,
+  `Materiales` mediumtext NOT NULL,
+  PRIMARY KEY (`idExamen`),
+  KEY `idMascotaExamen_idx` (`idMascota`),
+  CONSTRAINT `idMascotaExamen` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,6 +137,7 @@ CREATE TABLE `examen` (
 
 LOCK TABLES `examen` WRITE;
 /*!40000 ALTER TABLE `examen` DISABLE KEYS */;
+INSERT INTO `examen` VALUES (1,1,'Sangre','Examen de sangre','Estudio','Material3');
 /*!40000 ALTER TABLE `examen` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,30 +149,24 @@ DROP TABLE IF EXISTS `expedientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `expedientes` (
-  `idExpedientes` int NOT NULL AUTO_INCREMENT,
+  `idExpediente` int NOT NULL AUTO_INCREMENT,
   `idMascota` int NOT NULL,
-  `idUsuarios` int NOT NULL,
-  `idCita` int NOT NULL,
   `idConsulta` int NOT NULL,
-  `idVacuna` int NOT NULL,
-  `idExamen` int NOT NULL,
-  `idCirugia` int NOT NULL,
-  PRIMARY KEY (`idExpedientes`),
-  KEY `idMascota_idx` (`idMascota`),
-  KEY `idCita_idx` (`idCita`),
-  KEY `idUsuarios_idx` (`idUsuarios`),
-  KEY `idConsulta_idx` (`idConsulta`),
-  KEY `idExamen_idx` (`idExamen`),
+  `idExamen` int DEFAULT NULL,
+  `idVacuna` int DEFAULT NULL,
+  `idCirugia` int DEFAULT NULL,
+  PRIMARY KEY (`idExpediente`),
   KEY `idCirugia_idx` (`idCirugia`),
   KEY `idVacuna_idx` (`idVacuna`),
-  CONSTRAINT `fk_expedientes_idCita` FOREIGN KEY (`idCita`) REFERENCES `citas` (`idCita`),
-  CONSTRAINT `fk_expedientes_idConsulta` FOREIGN KEY (`idConsulta`) REFERENCES `consultas` (`idConsulta`),
-  CONSTRAINT `fk_expedientes_idUsuarios` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`idUsuarios`),
-  CONSTRAINT `fk_idMascota` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`),
-  CONSTRAINT `idCirugia` FOREIGN KEY (`idCirugia`) REFERENCES `cirugia` (`idcirugia`),
-  CONSTRAINT `idExamen` FOREIGN KEY (`idExamen`) REFERENCES `examen` (`idexamen`),
+  KEY `idExamen_idx` (`idExamen`),
+  KEY `idMascota_idx` (`idMascota`),
+  KEY `idConsulta_idx` (`idConsulta`),
+  CONSTRAINT `idCirugia` FOREIGN KEY (`idCirugia`) REFERENCES `cirugia` (`idCirugia`),
+  CONSTRAINT `idConsulta` FOREIGN KEY (`idConsulta`) REFERENCES `consultas` (`idConsulta`),
+  CONSTRAINT `idExamen` FOREIGN KEY (`idExamen`) REFERENCES `examen` (`idExamen`),
+  CONSTRAINT `idMascota` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`),
   CONSTRAINT `idVacuna` FOREIGN KEY (`idVacuna`) REFERENCES `vacuna` (`idVacuna`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,6 +175,7 @@ CREATE TABLE `expedientes` (
 
 LOCK TABLES `expedientes` WRITE;
 /*!40000 ALTER TABLE `expedientes` DISABLE KEYS */;
+INSERT INTO `expedientes` VALUES (1,1,1,NULL,NULL,1),(2,1,2,NULL,1,NULL),(3,1,4,1,NULL,NULL);
 /*!40000 ALTER TABLE `expedientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -200,18 +189,15 @@ DROP TABLE IF EXISTS `mascotas`;
 CREATE TABLE `mascotas` (
   `idMascota` int NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(50) NOT NULL,
-  `Edad` int NOT NULL,
+  `FechaNacimiento` date NOT NULL,
   `Especie` varchar(50) NOT NULL,
   `Raza` varchar(50) NOT NULL,
   `Sexo` varchar(45) NOT NULL,
-  `idExpedientes` int NOT NULL,
-  `idUsuarios` int NOT NULL,
+  `idUsuario` int NOT NULL,
   PRIMARY KEY (`idMascota`),
-  KEY `idExpedientes_idx` (`idExpedientes`),
-  KEY `idUsuarios_idx` (`idUsuarios`),
-  CONSTRAINT `idExpedientes` FOREIGN KEY (`idExpedientes`) REFERENCES `expedientes` (`idExpedientes`),
-  CONSTRAINT `idUsuarios` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`idUsuarios`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `idUsuarios_idx` (`idUsuario`),
+  CONSTRAINT `idUsuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -220,6 +206,7 @@ CREATE TABLE `mascotas` (
 
 LOCK TABLES `mascotas` WRITE;
 /*!40000 ALTER TABLE `mascotas` DISABLE KEYS */;
+INSERT INTO `mascotas` VALUES (1,'Misi','2022-02-10','Gato','Curl Americano','F',7),(2,'Misty','2022-04-26','Gato','American Wirehair','F',7),(3,'Raúl','2020-03-25','Gato','Europeo Doméstico','M',8),(4,'Bonnie','2021-03-15','Gato','Americano Pelo Corto','M',5),(5,'Canelo','2015-06-14','Perro','Nureongi','M',5),(6,'Tanathos','2021-10-07','Gato','Chartreux','M',6),(7,'Pandora','2023-08-07','Gato','Fold','F',6),(8,'Luis','2021-06-12','Iguana','runoceconte','M',8),(9,'Marlen','2023-09-20','Pez','Payaso','M',8);
 /*!40000 ALTER TABLE `mascotas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,8 +232,8 @@ CREATE TABLE `pagos` (
   PRIMARY KEY (`idPago`),
   KEY `idUsuarios_ipagos` (`idUsuarios`),
   KEY `idExpedientes_Pagos_idx` (`IdExpediente`),
-  CONSTRAINT `idExpedientes_Pagos` FOREIGN KEY (`IdExpediente`) REFERENCES `expedientes` (`idExpedientes`),
-  CONSTRAINT `idUsuarios_pagos` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`idUsuarios`)
+  CONSTRAINT `idExpedientes_Pagos` FOREIGN KEY (`IdExpediente`) REFERENCES `expedientes` (`idExpediente`),
+  CONSTRAINT `idUsuarios_pagos` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`idUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -318,14 +305,16 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `idUsuarios` int NOT NULL AUTO_INCREMENT,
-  `NombreUsuario` varchar(100) NOT NULL,
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) NOT NULL,
   `Telefono` varchar(15) NOT NULL,
-  `correo` varchar(100) NOT NULL,
+  `Correo` varchar(100) NOT NULL,
   `Direccion` varchar(255) NOT NULL,
-  `Rol` enum('Administrador','Veterinario','Dueno') NOT NULL,
-  PRIMARY KEY (`idUsuarios`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Rol` enum('Administrador','Veterinario','Dueño') NOT NULL,
+  `Usuario` varchar(50) NOT NULL,
+  `Contrasena` varchar(255) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -334,6 +323,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Dr. Gabriela Bautista','77548926','gabriela.bautista@gmail.com','Cojutepeque, Cuscatlan','Veterinario','GaBau','MeBa'),(2,'Dr. Roberto Alfaro','98765432','robert.alfa@gmail.com','San Juan Opico, La libertad','Veterinario','Rob','Alfa'),(3,'Dr. Cristian Vasquez','45678912','cristian.vasquez@gmail.com','Ilopango, San Salvador','Veterinario','Cris','Vas'),(4,'Dr. Esmeralda Castellanos','96385274','esmeralda.castellanos@gmail.com','San Marcos, San Salvador','Veterinario','Esme','Cas'),(5,'Maria Mejia','74185296','maria.mejia@gmail.com','Sta Cruz Michapa, Cuscatlan','Dueño','NixieNixi','wangxian'),(6,'Rafael Ramos','85296374','rafael.ramos@gmail.com','Santa Ana, Santa Ana','Dueño','Raff','RarMustis'),(7,'Mauricio Hernandez','12305678','mauricio.hernan@gmail.com','San Miguel Tepezontes,La Paz','Dueño','Mau2','Canela1'),(8,'Odeth Perez','75342189','odeth.perez@gmail.com','Sierra Morena,San Salvador','Dueño','Ruby','Ruby1'),(9,'Luis Escobar','12345678','luis.escobar@ues.edu.sv','San Salvador,San Salvador','Administrador','LuisEsco','Escobar12');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -346,17 +336,15 @@ DROP TABLE IF EXISTS `vacuna`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vacuna` (
   `idVacuna` int NOT NULL AUTO_INCREMENT,
-  `Tipo` varchar(100) NOT NULL,
-  `Descripcion` longtext,
-  `Motivo` varchar(255) DEFAULT NULL,
   `idMascota` int NOT NULL,
-  `idExpedientes` int NOT NULL,
+  `Tipo` enum('Perro Moquillo','Perro Parvovirus','Perro Hepatitis','Perro Leptospirosis','Gato Leucemia','Gato Calicivirus','Gato Herpesvirus','Gato Panleucopenia','Rabia') NOT NULL,
+  `Descripcion` longtext,
+  `Motivo` varchar(255) NOT NULL,
+  `Materiales` mediumtext NOT NULL,
   PRIMARY KEY (`idVacuna`),
-  KEY `idMascota_idx` (`idMascota`),
-  KEY `fk_idExpediente` (`idExpedientes`),
-  CONSTRAINT `fk_idExpediente` FOREIGN KEY (`idExpedientes`) REFERENCES `expedientes` (`idExpedientes`),
-  CONSTRAINT `idMascota` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `idMascotaVacuna_idx` (`idMascota`),
+  CONSTRAINT `idMascotaVacuna` FOREIGN KEY (`idMascota`) REFERENCES `mascotas` (`idMascota`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -365,6 +353,7 @@ CREATE TABLE `vacuna` (
 
 LOCK TABLES `vacuna` WRITE;
 /*!40000 ALTER TABLE `vacuna` DISABLE KEYS */;
+INSERT INTO `vacuna` VALUES (1,1,'Rabia',NULL,'Prevencion','Material1, Material2');
 /*!40000 ALTER TABLE `vacuna` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -377,4 +366,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-24 21:11:02
+-- Dump completed on 2024-10-27  0:07:01
