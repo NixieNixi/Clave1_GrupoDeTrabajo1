de7aaa01f6f49dcd64c443260db78fc830ecb0d5
Galleta <galletadecanela2048@gmail.com>
2024-11-10 13:50:38 -0600

    funcion cargar inventario

--- Administrador/AdminInventario.cs
+++ Administrador/AdminInventario.cs
@@ -263,7 +263,54 @@ namespace Clave1_GrupoDeTrabajo1.Administrador
 
         private void btnVerTodosI_Click(object sender, EventArgs e)
         {
+            CargarInventario();
+        }
 
+        /// <summary>
+        /// Metodo que carga los producto del inventario en el dgvInventario
+        /// </summary>
+        private void CargarInventario()
+        {
+            string queryInventario = @" SELECT idProductos, Nombre, Precio, Descripcion, Cantidad FROM productos;";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(MenuPrincipal.connectionString))
+                {
+                    using (MySqlCommand command = new MySqlCommand(queryInventario, connection))
+                    {
+                        connection.Open();
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            dgvInventario.Rows.Clear();
+
+                            List<Producto> productos = new List<Producto>();
+
+                            while (reader.Read())
+                            {
+                                Producto producto = new Producto
+                                {
+                                    IdProductos = reader.GetInt32("idProductos"),
+                                    Nombre = reader["Nombre"].ToString(),
+                                    Precio = reader.GetDecimal("Precio"),
+                                    Descripcion = reader["Descripcion"].ToString(),
+                                    Cantidad = reader.GetInt32("Cantidad")
+                                };
+
+                                productos.Add(producto);
+
+                                dgvInventario.Rows.Add(producto.IdProductos, producto.Nombre, producto.Precio, producto.Descripcion, producto.Cantidad
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexion a base de datos " + ex.Message);
+            }
         }
     }
 }
